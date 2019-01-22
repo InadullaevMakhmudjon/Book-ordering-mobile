236d609bb2753121ea005acef20409f17aa8bf95
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-01-22 04:53:02 -0800

    Registration Activity added

--- app/src/main/java/com/example/bookapplication/ui/mainactivity/ActivityMain.java
+++ app/src/main/java/com/example/bookapplication/ui/mainactivity/ActivityMain.java
@@ -16,7 +16,7 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 
     public void finishClick(View v){
